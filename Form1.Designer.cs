ff707d32da18f668b2c48e0b7edafc865a13d2a9
amazake13 <souamano@icloud.com>
2023-10-05 09:34:56 +0900

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Black_Jack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 314);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "HIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(6, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CROUPIER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(6, 350);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PLAYER";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(594, 314);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "STAND";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(179, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(179, 350);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 27);
+            this.label4.TabIndex = 7;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(494, 268);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(138, 31);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ThousandsSeparator = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(634, 270);
+            this.button3.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "BET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(555, 378);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(139, 34);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(555, 230);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(139, 34);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(488, 234);
+            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 24);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "BET";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(488, 382);
+            this.label6.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 24);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "CHIP";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(651, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 28);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "QUIT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(492, 15);
+            this.label7.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 24);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "HIGH\r\nSCORE";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(537, 17);
+            this.label8.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 15);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 424);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Name = "Form1";
+            this.Text = "Black Jack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Black_Jack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+
+        PictureBox[] cards = new PictureBox[52];
+        PictureBox back = new PictureBox();
+
+        int count = 0;
+        int croupier = 0;
+        int player = 0;
+        int sum_c = 0;
+        int sum_p = 0;
+        int _sum_c = 0;
+        int _sum_p = 0;
+        int A_c = 0;
+        int A_p = 0;
+        int chip = 50000;
+        int bet = 0;
+
+        private void Initialize()
+        {
+            for (int i = 0; i < 52; i++)
+                cards[i].Visible = false;
+            back.Visible = false;
+            
+            Shuffle(cards);
+            Image_Set(cards);
+
+            count = 0;
+            croupier = 0;
+            player = 0;
+            sum_c = 0;
+            sum_p = 0;
+            _sum_c = 0;
+            _sum_p = 0;
+            A_c = 0;
+            A_p = 0;
+        }
+
+        private void Ready()
+        {
+            Croupier_Draw_Card(cards);
+            back.Location = new Point(12 +  croupier * 30, 24);
+            back.Visible = true;
+            back.BringToFront();
+
+            Player_Draw_Card(cards);
+            Player_Draw_Card(cards);
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+    
+        private void Shuffle(PictureBox[] n)
+        {
+            int[] t = new int[52];
+            for (int i = 0; i < t.Length; i++)
+                t[i] = i;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                int random = rand.Next(0, 52);
+                int temp = t[i];
+                t[i] = t[random];
+                t[random] = temp;
+            }
+
+            int k = 0;
+            for (int i = 0; i < 52; i++)
+            {
+                n[i].Tag = t[k];
+                k++;
+            }
+        }
+
+        private void Image_Set(PictureBox[] n)
+        {
+            string[] suits = new string[4] { "s", "h", "d", "c" };
+
+            for (int i = 0; i < n.Length; i++)
+            { 
+                int tag = (int)n[i].Tag;
+                int no = tag % 13 + 1;
+                n[i].Image = (Image)Properties.Resources.ResourceManager.GetObject(suits[tag / 13] + no.ToString("00"));
+            }
+        }
+
+        private void Wait()
+        {
+            button4.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            numericUpDown1.Enabled = true;
+        }
+
+        
+
+        private void A_Sum(ref int s,int _s,int A)
+        {
+            int j = 0;
+            for (int i = A; i >= 0; i--)
+            {
+                s = _s + 11 * i + j++;
+                if (s <= 21)
+                    break;
+                else
+                    s = _s + j - 1;
+            }
+        }
+
+        private void Croupier_Draw_Card(PictureBox[] n)
+        {
+            n[count].Location = new Point(12 + croupier++ * 30, 24);
+            n[count].Visible = true;
+            n[count].BringToFront();
+           
+            if ((int)n[count].Tag % 13 + 1 >= 11)
+                _sum_c += 10;
+            else if ((int)n[count].Tag % 13 + 1 != 1)
+                _sum_c += (int)n[count].Tag % 13 + 1;
+            else
+                A_c++;
+
+            A_Sum(ref sum_c, _sum_c, A_c);
+
+            if (sum_c > 21)
+            {
+                label3.Text = sum_c.ToString();
+                MessageBox.Show("WIN");
+                chip += 2 * bet;
+                bet = 0;
+                textBox1.Text = chip.ToString();
+                textBox2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+            }
+            else
+                label3.Text = sum_c.ToString();
+
+            count++;
+        }
+
+        private void Player_Draw_Card(PictureBox[] n)
+        {
+            n[count].Location = new Point(12 + player++ * 30, 240);
+            n[count].Visible = true;
+            n[count].BringToFront();
+
+            if ((int)n[count].Tag % 13 + 1 >= 11)
+                _sum_p += 10;
+            else if ((int)n[count].Tag % 13 + 1 != 1)
+                _sum_p += (int)n[count].Tag % 13 + 1;
+            else
+                A_p++;
+
+            A_Sum(ref sum_p, _sum_p, A_p);
+
+            if (sum_p > 21)
+            {
+                back.Visible = false;
+                n[++count].Visible = true;
+                n[count].Location = new Point(42, 24);
+                n[count].BringToFront();
+
+                if ((int)n[count].Tag % 13 + 1 >= 11)
+                    _sum_c += 10;
+                else if ((int)n[count].Tag % 13 + 1 != 1)
+                    _sum_c += (int)n[count].Tag % 13 + 1;
+                else
+                    A_c++;
+
+                A_Sum(ref sum_c, _sum_c, A_c);
+
+                label3.Text = sum_c.ToString();
+                label4.Text = sum_p.ToString();
+                MessageBox.Show("LOSE");
+                bet = 0;
+                textBox2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                Initialize();
+                Wait();
+            }
+            else
+                label4.Text = sum_p.ToString();
+
+            count++;
+        }
+
+        private void Compare(int x,int y)
+        {
+            if (x == y)
+            {
+                MessageBox.Show("DRAW");
+                chip += bet;
+                bet = 0;
+                textBox1.Text = chip.ToString();
+                textBox2.Text = "";
+            }
+            else if (x > y)
+            {
+                MessageBox.Show("WIN");
+                chip += 2 * bet;
+                bet = 0;
+                textBox1.Text = chip.ToString();
+                textBox2.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("LOSE");
+                bet = 0;
+                textBox2.Text = "";
+            }
+
+            label3.Text = "";
+            label4.Text = "";
+            Initialize();
+            Wait();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label8.Text = Properties.Settings.Default.high_score.ToString();
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i] = new PictureBox();
+                cards[i].Size = new Size(70, 105);
+                cards[i].SizeMode = PictureBoxSizeMode.Zoom;
+                Controls.Add(cards[i]);
+            }
+
+            back.Size = new Size(70, 105);
+            back.SizeMode = PictureBoxSizeMode.Zoom;
+            back.Image = Properties.Resources.z01;
+            Controls.Add(back);
+
+            textBox1.Text = chip.ToString();
+            Wait();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Player_Draw_Card(cards);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            back.Visible = false;
+
+            while (true)
+            {
+                Croupier_Draw_Card(cards);
+                if (sum_c > 21)
+                {
+                    Initialize();
+                    Wait();
+                    break;
+                }
+                else if (sum_c >= 17)
+                {
+                    Compare(sum_p, sum_c);
+                    break;
+                }
+            }  
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(numericUpDown1.Value == 0)
+            {
+                numericUpDown1.Value = 0;
+            }
+            else if (chip - (int)numericUpDown1.Value >= 0)
+            {
+                chip -= (int)numericUpDown1.Value;
+                bet = (int)numericUpDown1.Value;
+                textBox1.Text = chip.ToString();
+                textBox2.Text = bet.ToString();
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button1.Enabled = true;
+                button2.Enabled = true;
+                numericUpDown1.Enabled = false;
+                Initialize();
+                Ready();
+            }
+            else
+            {
+                numericUpDown1.Value = 0;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (chip > Properties.Settings.Default.high_score)
+            {
+                Properties.Settings.Default.high_score = chip;
+                Properties.Settings.Default.Save();
+                label8.Text = Properties.Settings.Default.high_score.ToString();
+                chip = 50000;
+                textBox1.Text = chip.ToString();
+                Initialize();
+                Wait();
+            }
+            else
+            {
+                chip = 50000;
+                textBox1.Text = chip.ToString();
+                Initialize();
+                Wait();
+            }
+                
+        }
+    }
+}
